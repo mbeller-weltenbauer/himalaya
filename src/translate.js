f7732faa293df064d9341d047ef4fcf8fa3d331f
Martin Beller <martin.beller@weltenbauer-se.com>
2017-12-13 15:15:50 +0100

    martins special fix...

--- src/translate.js
+++ src/translate.js
@@ -1,206 +1,224 @@
-import paul from 'paul'
-import {
-  stringIncludes,
-  arrayIncludes
-} from './compat'
+'use strict';
+
+var _paul = require('paul');
+
+var _paul2 = _interopRequireDefault(_paul);
+
+var _compat = require('./compat');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // c/p'd from ../index.js
-const voidTags = [
-  '!doctype', 'area', 'base', 'br', 'col', 'command',
-  'embed', 'hr', 'img', 'input', 'keygen', 'link',
-  'meta', 'param', 'source', 'track', 'wbr'
-]
+var voidTags = ['!doctype', 'area', 'base', 'br', 'col', 'command', 'embed', 'hr', 'img', 'input', 'keygen', 'link', 'meta', 'param', 'source', 'track', 'wbr'];
 
-function serializeAttr (attr, value, isXml) {
-  if (!isXml && attr === value) return attr
-  const text = value.toString()
-  const quoteEscape = text.indexOf('\'') !== -1
-  const quote = quoteEscape ? '"' : '\''
-  return attr + '=' + quote + text + quote
+function serializeAttr(attr, value, isXml) {
+  if (!isXml && attr === value) return attr;
+  var text = value.toString();
+  var quoteEscape = text.indexOf('\'') !== -1;
+  var quote = quoteEscape ? '"' : '\'';
+  return attr + '=' + quote + text + quote;
 }
 
 // stolen from underscore.string
-function dasherize (str) {
-  return str.trim()
-    .replace(/([A-Z])/g, '-$1')
-    .replace(/[-_\s]+/g, '-')
-    .toLowerCase()
+function dasherize(str) {
+  return str.trim().replace(/([A-Z])/g, '-$1').replace(/[-_\s]+/g, '-').toLowerCase();
 }
 
-function inlineStyle (style) {
-  return Object.keys(style).reduce((css, key) => {
-    return `${css}; ${dasherize(key)}: ${style[key]}`
-  }, '').slice(2)
+function inlineStyle(style) {
+  return Object.keys(style).reduce(function (css, key) {
+    return css + '; ' + dasherize(key) + ': ' + style[key];
+  }, '').slice(2);
 }
 
-const htmlDefaults = {}
+var htmlDefaults = {};
 
-function toHTML (tree, options = htmlDefaults) {
-  const {doctype} = options
-  const isXml = doctype === 'xml'
-  const html = paul.walk(tree, (node, walk) => {
-    const {type, tagName, attributes, content} = node
-    if (type === 'Text') return content
-    if (type === 'Comment') return `<!--${content}-->`
-    let tag = '<' + tagName
-    for (const attr in attributes) {
-      const val = attributes[attr]
+function toHTML(tree) {
+  var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : htmlDefaults;
+  var doctype = options.doctype;
+
+  var isXml = doctype === 'xml';
+  var html = _paul2.default.walk(tree, function (node, walk) {
+
+    if(!node){
+      return;
+    }
+    var type = node.type,
+      tagName = node.tagName,
+      attributes = node.attributes,
+      content = node.content;
+
+    if (type.toLowerCase() === 'text'){
+      return content;
+    }
+    if (type === 'Comment') return '<!--' + content + '-->';
+    var tag = '<' + tagName;
+    for (var attr in attributes) {
+      var val = attributes[attr];
       if (attr === 'dataset') {
-        for (const prop in val) {
-          const key = 'data-' + dasherize(prop)
-          tag += ' ' + serializeAttr(key, val[prop], isXml)
+        for (var prop in val) {
+          var key = 'data-' + dasherize(prop);
+          tag += ' ' + serializeAttr(key, val[prop], isXml);
         }
-        continue
+        continue;
       }
 
       if (attr === 'style') {
-        tag += ' ' + serializeAttr(attr, inlineStyle(val))
-        continue
+        tag += ' ' + serializeAttr(attr, inlineStyle(val));
+        continue;
       }
 
       if (attr === 'className') {
-        tag += ' ' + serializeAttr('class', val.join(' '))
-        continue
+        tag += ' ' + serializeAttr('class', val.join(' '));
+        continue;
       }
 
-      tag += ' ' + serializeAttr(dasherize(attr), val, isXml)
+      tag += ' ' + serializeAttr(dasherize(attr), val, isXml);
     }
 
-    tag += '>'
-    const autoClose = !isXml && arrayIncludes(voidTags, tagName.toLowerCase())
-    if (autoClose) return tag
+    tag += '>';
+    var autoClose = !isXml && (0, _compat.arrayIncludes)(voidTags, tagName.toLowerCase());
+    if (autoClose) return tag;
 
-    const innerds = walk(node.children || []).join('')
-    return tag + innerds + `</${tagName}>`
-  })
-  if (html.join) return html.join('')
-  return html
+    var innerds = walk(node.children || []).join('');
+    return tag + innerds + ('</' + tagName + '>');
+  });
+  if (html.join) return html.join('');
+  return html;
 }
 
-const newline = '\n'
-const jadeDefaults = {
+var newline = '\n';
+var jadeDefaults = {
   indentation: '  '
+};
+
+function isWhitespaceNode(node) {
+  return !(node.type === 'Text' && !node.content.trim());
 }
 
-function isWhitespaceNode (node) {
-  return !(node.type === 'Text' && !node.content.trim())
-}
+function toJade(tree) {
+  var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : jadeDefaults;
+  var doctype = options.doctype;
 
-function toJade (tree, options = jadeDefaults) {
-  let {doctype} = options
-  const multi = multilineText(options.indentation)
+  var multi = multilineText(options.indentation);
 
-  if (tree.filter) tree = tree.filter(isWhitespaceNode)
-  const jade = paul.walk(tree, (node, walk, depth) => {
-    const {type, tagName, attributes} = node
+  if (tree.filter) tree = tree.filter(isWhitespaceNode);
+  var jade = _paul2.default.walk(tree, function (node, walk, depth) {
+    var type = node.type,
+      tagName = node.tagName,
+      attributes = node.attributes;
+
     if (type === 'Text') {
-      return multi(node.content, depth, '| ')
+      return multi(node.content, depth, '| ');
     }
     if (type === 'Comment') {
-      const text = node.content
-      return ~text.indexOf(newline)
-        ? multi('//', depth) + newline + multi(text, depth + 1)
-        : multi('//' + text, depth)
+      var text = node.content;
+      return ~text.indexOf(newline) ? multi('//', depth) + newline + multi(text, depth + 1) : multi('//' + text, depth);
     }
-    let tag = tagName
-    const {id, className} = attributes
-    if (id) tag += `#${id}`
-    if (className) tag += `.${className.join('.')}`
+    var tag = tagName;
+    var id = attributes.id,
+      className = attributes.className;
 
-    const redundantDiv = node.tagName === 'div' && tag.length > 3
-    if (redundantDiv) tag = tag.slice(3)
+    if (id) tag += '#' + id;
+    if (className) tag += '.' + className.join('.');
 
-    tag = multi(tag, depth)
-    const attrs = node.attributes
-    const props = Object.keys(attrs).filter(key => {
-      return key !== 'className' && key !== 'id'
-    })
+    var redundantDiv = node.tagName === 'div' && tag.length > 3;
+    if (redundantDiv) tag = tag.slice(3);
+
+    tag = multi(tag, depth);
+    var attrs = node.attributes;
+    var props = Object.keys(attrs).filter(function (key) {
+      return key !== 'className' && key !== 'id';
+    });
     if (props.length) {
-      const isXml = doctype === 'xml'
-      tag += '('
-      tag += props.map(prop => {
-        const val = attrs[prop]
+      var isXml = doctype === 'xml';
+      tag += '(';
+      tag += props.map(function (prop) {
+        var val = attrs[prop];
         if (prop === 'dataset') {
-          return Object.keys(val).map(attr => {
-            return serializeAttr('data-' + dasherize(attr), val[attr], isXml)
-          }).join(', ')
+          return Object.keys(val).map(function (attr) {
+            return serializeAttr('data-' + dasherize(attr), val[attr], isXml);
+          }).join(', ');
         }
-        if (prop === 'style') return serializeAttr(prop, inlineStyle(val))
-        return serializeAttr(dasherize(prop), val, isXml)
-      }).join(', ')
-      tag += ')'
+        if (prop === 'style') return serializeAttr(prop, inlineStyle(val));
+        return serializeAttr(dasherize(prop), val, isXml);
+      }).join(', ');
+      tag += ')';
     }
-    const lowTagName = node.tagName.toLowerCase()
-    if (arrayIncludes(voidTags, lowTagName)) {
+    var lowTagName = node.tagName.toLowerCase();
+    if ((0, _compat.arrayIncludes)(voidTags, lowTagName)) {
       if (lowTagName === '!doctype') {
-        if (!doctype) doctype = doctypeShortcut(tag)
-        return multi('doctype ' + doctype, depth)
+        if (!doctype) doctype = doctypeShortcut(tag);
+        return multi('doctype ' + doctype, depth);
       }
-      return tag
+      return tag;
     }
 
-    const {children} = node
-    if (!children.length) return tag
+    var children = node.children;
+
+    if (!children.length) return tag;
     if (children.length === 1 && children[0].type === 'Text') {
-      const text = children[0].content
-      return ~text.indexOf(newline)
-        ? tag + '.' + newline + multi(text, depth + 1)
-        : tag + ' ' + text
+      var _text = children[0].content;
+      return ~_text.indexOf(newline) ? tag + '.' + newline + multi(_text, depth + 1) : tag + ' ' + _text;
     }
 
-    return tag + newline +
-      walk(children.filter(isWhitespaceNode), depth + 1).join(newline)
-  }, 0)
-  if (jade.join) return jade.join(newline)
-  return jade
+    return tag + newline + walk(children.filter(isWhitespaceNode), depth + 1).join(newline);
+  }, 0);
+  if (jade.join) return jade.join(newline);
+  return jade;
 }
 
-function multilineText (indentation) {
-  let format = line => line
-  const hasTab = stringIncludes(indentation, '\t')
+function multilineText(indentation) {
+  var format = function format(line) {
+    return line;
+  };
+  var hasTab = (0, _compat.stringIncludes)(indentation, '\t');
   if (hasTab) {
-    format = line => line.replace(/\t/g, indentation)
+    format = function format(line) {
+      return line.replace(/\t/g, indentation);
+    };
   }
 
-  function indent (depth, str) {
+  function indent(depth, str) {
     while (depth--) {
-      str = indentation + str
+      str = indentation + str;
     }
-    return str
+    return str;
   }
 
-  return function (str, depth, lead = '') {
-    const lines = str
-      .split(newline)
-      .map(format)
-      .filter(line => !!line.trim())
+  return function (str, depth) {
+    var lead = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : '';
 
-    const start = maxSharedIndent(lines)
-    return lines
-      .map(line => indent(depth, lead + line.slice(start)))
-      .join(newline)
-  }
+    var lines = str.split(newline).map(format).filter(function (line) {
+      return !!line.trim();
+    });
+
+    var start = maxSharedIndent(lines);
+    return lines.map(function (line) {
+      return indent(depth, lead + line.slice(start));
+    }).join(newline);
+  };
 }
 
-function maxSharedIndent (lines) {
+function maxSharedIndent(lines) {
   return lines.reduce(function (num, line) {
-    return Math.min(num, line.length - line.trimLeft().length)
-  }, Infinity)
+    return Math.min(num, line.length - line.trimLeft().length);
+  }, Infinity);
 }
 
 // see http://jade-lang.com/reference/doctype/
-function doctypeShortcut (str) {
-  if (stringIncludes(str, 'Transitional')) return 'transitional'
-  if (stringIncludes(str, 'strict')) return 'strict'
-  if (stringIncludes(str, 'Frameset')) return 'frameset'
-  if (stringIncludes(str, 'Basic')) return 'basic'
-  if (stringIncludes(str, '1.1')) return '1.1'
-  if (stringIncludes(str, 'Mobile')) return 'mobile'
-  return 'html'
+function doctypeShortcut(str) {
+  if ((0, _compat.stringIncludes)(str, 'Transitional')) return 'transitional';
+  if ((0, _compat.stringIncludes)(str, 'strict')) return 'strict';
+  if ((0, _compat.stringIncludes)(str, 'Frameset')) return 'frameset';
+  if ((0, _compat.stringIncludes)(str, 'Basic')) return 'basic';
+  if ((0, _compat.stringIncludes)(str, '1.1')) return '1.1';
+  if ((0, _compat.stringIncludes)(str, 'Mobile')) return 'mobile';
+  return 'html';
 }
 
 module.exports = {
-  toHTML,
-  toJade,
+  toHTML: toHTML,
+  toJade: toJade,
   toPug: toJade
-}
+};
+//# sourceMappingURL=translate.js.map
